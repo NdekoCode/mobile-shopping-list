c8413312c91f670177f6a180c7b5da7a77c48d7d
Ndekocode <arickbulakali@gmail.com>
2024-02-17 22:38:04 +0200

    add container

--- styles/globals.js
+++ styles/globals.js
@@ -4,6 +4,13 @@ const styles  = StyleSheet.create({
     appView:{
         width:'100%',
         height: '1005'
+    },
+    container:{
+        width:'100%',
+        height: '100%',
+        flexDirection: 'column',
+        alignItems: 'center',
+        justifyContent:'center'
     }
 })
 
