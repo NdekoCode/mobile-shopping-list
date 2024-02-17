92f9f40e077d72e6726d904bc841afdcd867f0b7
Ndekocode <arickbulakali@gmail.com>
2024-02-17 22:36:00 +0200

    initial project

---
+++ App.js
@@ -0,0 +1,11 @@
+import { StatusBar } from 'expo-status-bar';
+import { StyleSheet, Text, View } from 'react-native';
+import styles from './styles/globals';
+
+export default function App() {
+  return (
+    <View style={styles.container}>
+      <Text>Hello shopping card</Text>
+    </View>
+  );
+}
---
+++ styles/globals.js
@@ -0,0 +1,10 @@
+import { StyleSheet } from "react-native";
+
+const styles  = StyleSheet.create({
+    appView:{
+        width:'100%',
+        height: '1005'
+    }
+})
+
+export default styles;
