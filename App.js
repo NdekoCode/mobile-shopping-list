35d8f4121da1b0b0584192ec4bca531a1b21b75d
Ndekocode <arickbulakali@gmail.com>
2024-02-19 22:19:06 +0200

    feat: initial app

--- App.js
+++ App.js
@@ -1,11 +1,39 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
-import styles from './styles/globals';
+import { Button, Text, TextInput, View } from "react-native";
+import styles from "./styles/globals";
+import { useState } from "react";
 
 export default function App() {
+  const [productName, setProductName] = useState("");
+  const [products, setProducts] = useState([]);
+  const handleTextChange = (text) => {
+    setProductName(text);
+  };
+  const handlePress = (e) => {
+    console.log("Button pressed");
+    setProducts((prevState) => [...prevState, productName]);
+  };
   return (
     <View style={styles.container}>
-      <Text>Hello shopping card</Text>
+      <View style={styles.viewText}>
+        <Text style={styles.textTitle}>Hello shopping card</Text>
+      </View>
+      <View style={styles.inputContainer}>
+        <TextInput
+          value={productName}
+          style={styles.textInput}
+          placeholder="Enter the name of the product"
+          onChangeText={handleTextChange}
+        ></TextInput>
+        <Button title="Valider" style={styles.button} onPress={handlePress} />
+      </View>
+      <View>
+        {products.length > 0 &&
+          products.map((product, index) => (
+            <View key={index}>
+              <Text> Name of the product: {product}</Text>
+            </View>
+          ))}
+      </View>
     </View>
   );
 }
--- styles/globals.js
+++ styles/globals.js
@@ -1,17 +1,45 @@
 import { StyleSheet } from "react-native";
 
-const styles  = StyleSheet.create({
-    appView:{
-        width:'100%',
-        height: '1005'
-    },
-    container:{
-        width:'100%',
-        height: '100%',
-        flexDirection: 'column',
-        alignItems: 'center',
-        justifyContent:'center'
-    }
-})
+const styles = StyleSheet.create({
+  container: {
+    padding: 20,
+    paddingTop: 60,
+  },
+
+  blockContent: {
+    width: "100%",
+    display: "flex",
+    gap: 10,
+    flexDirection: "column",
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  viewText: {
+    paddingBottom: 20,
+  },
+  inputContainer: {
+    flexDirection: "row",
+    alignItems: "stretch",
+    justifyContent: "center",
+  },
+  button: {
+    display: "flex",
+    justifyContent: "center",
+    alignItems: "center",
+  },
+  textInput: {
+    backgroundColor: "#fff",
+    borderWidth: 1,
+    borderColor: "#eeeeee",
+    borderStyle: "solid",
+    padding: 10,
+    borderRadius: 5,
+    flexGrow: 1,
+  },
+  textTitle: {
+    fontSize: 40,
+    fontWeight: "500",
+  },
+});
 
 export default styles;
