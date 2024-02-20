fb53014395d2245bb367e03ab5d672f9e70eaf58
Ndekocode <arickbulakali@gmail.com>
2024-02-20 23:01:54 +0200

    feat: add modal

--- components/ProductForm.js
+++ components/ProductForm.js
@@ -1,9 +1,10 @@
-import { Alert, Button, TextInput, View } from "react-native";
+import { Alert, Button, Modal, Text, TextInput, View } from "react-native";
 import styles from "../styles/globals";
 import { useEffect, useState } from "react";
 
 export const ProductForm = ({ handlePress, clearProducts }) => {
   const [count, setCount] = useState(0);
+  const [isValid, setIsValid] = useState(false);
   const [product, setProduct] = useState({
     name: "",
     id: Date.now().toString(),
@@ -29,6 +30,7 @@ export const ProductForm = ({ handlePress, clearProducts }) => {
         name: "",
         id: "",
       });
+      setIsValid(true);
     } else {
       setCount((c) => c + 1);
       Alert.alert(
@@ -51,19 +53,29 @@ export const ProductForm = ({ handlePress, clearProducts }) => {
     }
   };
   return (
-    <View style={styles.inputContainer}>
-      <TextInput
-        value={product.name}
-        style={styles.textInput}
-        placeholder="Enter the name of the product"
-        onChangeText={handleTextChange}
-      ></TextInput>
-      <Button
-        disabled={product.name.trim().length < 2}
-        title="Valider"
-        style={styles.button}
-        onPress={handleSubmit}
-      />
-    </View>
+    <>
+      <View style={styles.inputContainer}>
+        <TextInput
+          value={product.name}
+          style={styles.textInput}
+          placeholder="Enter the name of the product"
+          onChangeText={handleTextChange}
+        ></TextInput>
+        <Button
+          disabled={product.name.trim().length < 2}
+          title="Valider"
+          style={styles.button}
+          onPress={handleSubmit}
+        />
+      </View>
+      <Modal
+        visible={isValid}
+        style={{ height: "auto" }}
+        onRequestClose={() => setIsValid(false)}
+      >
+        <Text>Votre produit a eté ajouter</Text>
+        <Button title="Close" onPress={() => setIsValid(false)} />
+      </Modal>
+    </>
   );
 };
